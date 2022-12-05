187eaaa26a70969e73f47530be0846a984d50eab
Programmer-Sourav <appsbucket333@gmail.com>
2022-12-05 16:43:11 +0530

    fix updated code

--- areaOftriangle.js
+++ areaOftriangle.js
@@ -6,7 +6,13 @@ check_area.addEventListener("click", calculateAreaOfATriangle)
 
 
 function calculateAreaOfATriangle(){
+
+if(Number(base_and_height[0].value)<=0 || Number(base_and_height[1].value)<=0){
+        outputInfo.innerText = "Values should be more than zero";
+}   
+else{ 
 const areaOfTriangle = (Number(base_and_height[0].value) * Number(base_and_height[1].value))/2 
 console.log(areaOfTriangle)
 outputInfo.innerText = "Area of a Triangle "+areaOfTriangle
+    }
 }
